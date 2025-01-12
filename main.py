6b981892640c966b54d9f20020e10108598aeb86
Bright Alour <brightalour@gmail.com>
2025-01-12 08:51:59 +0000

    added conditional function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+from typing import Literal
 from typing import TypedDict
 
 class State(TypedDict):
@@ -15,3 +17,19 @@ def node_2(state):
 def node_3(state):
     print("---Node 3---")
     return {"graph_state": state['graph_state'] +" sad!"}
+  
+  
+#Conditionally decides which node to visit next
+def decide_mood(state) -> Literal["node_2", "node_3"]:
+    
+    # Often, we will use state to decide on the next node to visit
+    user_input = state['graph_state'] 
+    
+    # Here, let's just do a 50 / 50 split between nodes 2, 3
+    if random.random() < 0.5:
+
+        # 50% of the time, we return Node 2
+        return "node_2"
+    
+    # 50% of the time, we return Node 3
+    return "node_3"  
