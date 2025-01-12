10cb7a65dc3d534b4d1a1e922e3e52c2523cb2e3
Bright Alour <brightalour@gmail.com>
2025-01-12 08:50:17 +0000

    added node functions

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ from typing import TypedDict
 
 class State(TypedDict):
   graph_state: str
-  
+ 
+#Adds a string to the state message  
+def node_1(state):
+    print("---Node 1---")
+    return {"graph_state": state['graph_state'] +" I am"}
+#Adds a string to the state message  
+def node_2(state):
+    print("---Node 2---")
+    return {"graph_state": state['graph_state'] +" happy!"}
+#Adds a string to the state message  
+def node_3(state):
+    print("---Node 3---")
+    return {"graph_state": state['graph_state'] +" sad!"}
