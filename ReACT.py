da39419f650aee6262ecb5882f56724065f042e6
Bright Alour <brightalour@gmail.com>
2025-01-14 07:56:37 +0000

    MessagesState understood - largely

--- ReACT.py
+++ ReACT.py
@@ -1,4 +1,7 @@
 from langchain_groq import ChatGroq
+from langgraph.graph import MessagesState  # Predefined state schema with a single parameter: a list of messages. 
+# As well as logic to append messages to the state at each node or edge
+from langchain_core.messages import HumanMessage, SystemMessage  # Prompt templates
 
 # These 3 functions will be bound to the model as tools
 
@@ -38,3 +41,11 @@ llm = ChatGroq(model_name="llama-3.3-70b-versatile")
 
 # Bind the tools to the model
 llm_with_tools = llm.bind_tools(tools, parallel_tool_calls=False)
+
+
+# System message
+sys_msg = SystemMessage(content="You are a helpful assistant tasked with performing arithmetic on a set of inputs.")
+
+# Node
+def assistant(state: MessagesState): # The state is a list of messages
+   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]} # Add the new messages to the system message and invoke the model_with_tools
